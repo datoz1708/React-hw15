0450f4da18c80c1d3d47ae3db020e35d3bf5225d
datoz1708 <Dato.zarandia.1@gmail.com>
2024-10-17 01:24:20 +0400

    ok

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React from 'react';
-import useSum from './UseSum';
+import UseSum from './UseSum';
 
 const App = () => {
   const num1 = 5;
   const num2 = 10;
 
-  useSum(num1, num2);
-
-  return (
-    <div>
-      <h1>Check the console for the sum!</h1>
-    </div>
-  );
+  UseSum(num1, num2);
 };
 
 export default App;
